026359b8948a1f5e7c2c1f89cf940b330086483b
SepsiLaszlo <sepsi.laszlo99@gmail.com>
2021-05-09 12:42:50 -0700

    remove arguments from HttpRepository

--- app/controllers/pull_requests_controller.rb
+++ app/controllers/pull_requests_controller.rb
@@ -1,6 +1,5 @@
 class PullRequestsController < ApplicationController
   def index
-    http_repository = HttpRepository.instance
-    @pull_requests = http_repository.get_pull_requests
+    @pull_requests = HttpRepository.get_pull_requests
   end
 end
--- app/services/http_repository.rb
+++ app/services/http_repository.rb
@@ -1,25 +1,19 @@
 class HttpRepository
-  attr_accessor :url
-
-  BASE_URL = 'https://api.github.com/repos'
+  URL = 'https://api.github.com/repos/SepsiLaszlo/tc-rails-crawler'
   HEADERS = {
     "Authorization" => "token #{ENV["GITHUB_OAUTH_TOKEN"]}"
   }
 
-  def initialize(owner, repo)
-    @url = "#{BASE_URL}/#{owner}/#{repo}"
-  end
-
-  def get_pull_requests
-    raw_pull_requests = HTTParty.get(url + '/pulls', headers: HEADERS)
+  def self.get_pull_requests
+    raw_pull_requests = HTTParty.get(URL + '/pulls', headers: HEADERS)
     raw_pull_requests.map do |pull_request|
       PullRequest.new(number: pull_request['number'], html_url: pull_request['html_url'])
     end
   end
 
   # commits listed in "#{url}/pulls/#{number}/commits" does not contain the changed file array
-  def get_commits_for_pull_request(number)
-    raw_commits = HTTParty.get("#{url}/pulls/#{number}/commits", headers: HEADERS)
+  def self.get_commits_for_pull_request(number)
+    raw_commits = HTTParty.get("#{URL}/pulls/#{number}/commits", headers: HEADERS)
     commit_sha_s = raw_commits.map do |commit|
       commit['sha']
     end
@@ -27,12 +21,13 @@ class HttpRepository
       raw_commit = get_commit(commit_sha)
       Commit.new(
         sha: raw_commit['sha'],
-        file_names: raw_commit['files'].map{|file| file['filename']}
+        file_names: raw_commit['files'].map{|file| file['filename']},
+        html_url: raw_commit['html_url']
       )
     end
   end
 
-  def get_commit(sha)
-    HTTParty.get("#{url}/commits/#{sha}", headers: HEADERS)
+  def self.get_commit(sha)
+    HTTParty.get("#{URL}/commits/#{sha}", headers: HEADERS)
   end
 end
