91d812d02df1eb836099103bbff2e459520248e2
SepsiLaszlo <sepsi.laszlo99@gmail.com>
2021-05-10 04:08:31 -0700

    refactor token management

--- README.md
+++ README.md
@@ -8,6 +8,17 @@ This application crawls the official Rails repository throughout the GitHub API.
 ```
 
 ## Set up
+The application can exceed the open request limit on the GiHub API.
+Provide an OAuth Personal Access Token in the `.env` file to allow more request for the application. Basic repository read rights are enough.
+
+Create the `.env` file.
+
+```sh
+cp .env.example .env
+```
+
+Then paste in your token.
+
 The application uses `ruby 2.7.1`. You can install the correct ruby version natively or use it with RVM:
 ```sh
 rvm use 2.7.1
--- app/services/http_repository.rb
+++ app/services/http_repository.rb
@@ -1,13 +1,16 @@
 class HttpRepository
   URL = 'https://api.github.com/repos/rails/rails'
 
-  HEADERS = {
-    "Authorization" => "token #{ENV["GITHUB_OAUTH_TOKEN"]}"
-  }
+  def self.headers
+    token = ENV["GITHUB_OAUTH_TOKEN"]
+    return {} if token.nil? || token.empty?
+
+    { "Authorization" => "token #{token}" }
+  end
 
   def self.get_pull_requests
     Rails.logger.info 'GET PULL REQUESTS'
-    pull_requests_json = HTTParty.get(URL + '/pulls', headers: HEADERS)
+    pull_requests_json = HTTParty.get(URL + '/pulls', headers: headers)
     pull_requests_json.map do |pull_request_json|
       PullRequest.create_from_json(pull_request_json)
     end
@@ -15,13 +18,13 @@ class HttpRepository
 
   def self.get_pull_request(number)
     Rails.logger.info "GET PULL REQUEST #{number}"
-    pull_request_json = HTTParty.get("#{URL}/pulls/#{number}", headers: HEADERS)
+    pull_request_json = HTTParty.get("#{URL}/pulls/#{number}", headers: headers)
     PullRequest.create_from_json(pull_request_json)
   end
 
   # commits listed in "#{url}/pulls/#{number}/commits" does not contain the changed file array
   def self.get_commits_for_pull_request(number)
-    raw_commits = HTTParty.get("#{URL}/pulls/#{number}/commits", headers: HEADERS)
+    raw_commits = HTTParty.get("#{URL}/pulls/#{number}/commits", headers: headers)
     commit_sha_s = raw_commits.map do |commit|
       commit['sha']
     end
@@ -33,6 +36,6 @@ class HttpRepository
 
   def self.get_commit(sha)
     Rails.logger.info "GET COMMIT #{sha}"
-    HTTParty.get("#{URL}/commits/#{sha}", headers: HEADERS)
+    HTTParty.get("#{URL}/commits/#{sha}", headers: headers)
   end
 end
