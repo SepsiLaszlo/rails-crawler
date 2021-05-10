d0b15732e9af49cf81c61a9464cc340d1a5f6740
SepsiLaszlo <sepsi.laszlo99@gmail.com>
2021-05-10 01:54:52 -0700

    add sort to PullRequestController index

--- app/controllers/pull_requests_controller.rb
+++ app/controllers/pull_requests_controller.rb
@@ -1,5 +1,8 @@
 class PullRequestsController < ApplicationController
   def index
     @pull_requests = HttpRepository.get_pull_requests
+    @pull_requests = @pull_requests.sort_by do |pull_request|
+      pull_request.files_changed_multiple_times? ? 0 : 1
+    end
   end
 end
