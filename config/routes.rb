f7a665361609fd83054df84de63c24d68ab0046d
SepsiLaszlo <sepsi.laszlo99@gmail.com>
2021-05-10 01:52:27 -0700

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'pulls', to: 'pull_requests#index'
+  get 'pulls/:number', to: 'pull_requests#show', as: :pull
   root "pull_requests#index"
 end
